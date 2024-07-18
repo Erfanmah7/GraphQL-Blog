d89f9e6019bfd993bf593d53e0d12cfca413017c
Erfanmah7 <efn8mah@yahoo.com>
2024-07-18 21:18:12 +0330

    add blog & author

---
+++ src/components/home/HomePage.jsx
@@ -0,0 +1,26 @@
+import { Container, Grid, Typography } from "@mui/material";
+import Author from "../author/Author";
+import Blog from "../blog/Blog";
+
+function HomePage() {
+  return (
+    <Container maxWidth="lg">
+      <Grid container padding={3}>
+        <Grid item xs={12} md={3} mt={4}>
+          <Typography variant="h5" component="h3" mb={3} fontWeight={700}>
+            نویسنده ها
+          </Typography>
+          <Author />
+        </Grid>
+        <Grid item xs={12} md={9} mt={4}>
+          <Typography variant="h5" component="h3" mb={3} fontWeight={700}>
+            مقالات
+          </Typography>
+          <Blog />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/clyn92owa00fs07w3str5jdw3/master",
+  cache: new InMemoryCache(),
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>
+  // </React.StrictMode>
+);
