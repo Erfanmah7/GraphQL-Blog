ffe34ee57ec8c927c9ffba431d94975bdfe228da
Erfanmah7 <efn8mah@yahoo.com>
2024-07-22 19:43:57 +0330

    add blog content

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,7 +1,72 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { GET_POST } from "../../graphql/queries";
+import Loader from "../shared/Loader";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import sanitizeHtml from "sanitize-html";
 
 function BlogPage() {
-  return <div>BlogPage</div>;
+  const navigate = useNavigate();
+  const { slug } = useParams();
+  const { loading, data, errors } = useQuery(GET_POST, { variables: { slug } });
+  if (loading) return <Loader />;
+  if (errors) return <h3>error...</h3>;
+  console.log(data);
+  return (
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          mt={9}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography
+            component="h2"
+            variant="h4"
+            color="primary"
+            fontWeight={700}
+          >
+            {data.post.title}
+          </Typography>
+          <ArrowBackRoundedIcon onClick={() => navigate(-1)} />
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <img
+            src={data.post.coverPhoto.url}
+            alt={data.post.slug}
+            width="100%"
+            style={{ borderRadius: 15 }}
+          />
+        </Grid>
+        <Grid item xs={12} mt={7} display="flex" alignItems="center">
+          <Avatar
+            src={data.post.author.avator.url}
+            sx={{ width: 80, height: 80, marginLeft: 2 }}
+          />
+          <Box component="div">
+            <Typography component="p" variant="h5" fontWeight={700}>
+              {data.post.author.name}
+            </Typography>
+            <Typography component="p" variant="p" color="text.secondary">
+              {data.post.author.field}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} mt={5}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: sanitizeHtml(data.post.content.html),
+            }}
+          ></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default BlogPage;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -56,4 +56,25 @@ const GET_AUTHOR = gql`
   }
 `;
 
-export { GET_POSTS, GET_AUTHORS, GET_AUTHOR };
+const GET_POST = gql`
+  query getPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      title
+      coverPhoto {
+        url
+      }
+      content {
+        html
+      }
+      author {
+        avator {
+          url
+        }
+        field
+        name
+      }
+    }
+  }
+`;
+
+export { GET_POSTS, GET_AUTHORS, GET_AUTHOR, GET_POST };
